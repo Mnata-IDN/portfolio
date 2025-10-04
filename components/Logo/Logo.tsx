bdda2e6d705b478b7f83c9acb6a42a2b414b8f4e
Sedenter <bagasuwaidha007@gmail.com>
2025-10-04 17:08:52 +0700

    Update Logo.tsx

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default const SVGComponent = (props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
+export const SVGComponent = (props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
   <Link href='/'>
     <svg
       xmlns="http://www.w3.org/2000/svg"
